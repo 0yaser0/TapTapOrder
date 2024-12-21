7104ccb3a15326c641fb1af5dccd158f4776339a
Yaser yjjou <125563605+0yaser0@users.noreply.github.com>
2024-12-21 13:39:59 +0100

    test the toolBar

--- app/src/main/java/com/cmc/taptaporders/MainActivity.kt
+++ app/src/main/java/com/cmc/taptaporders/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.cmc.taptaporders.ui.theme.TapTaOrdersTheme
+import com.example.taptaorders.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +23,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TapTaOrdersTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { MyToolbar() }
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -28,6 +35,37 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyToolbar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.taptaporder),
+                modifier = Modifier.padding(horizontal = 20.dp)
+            )
+        },
+        actions = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(R.drawable.ic_dark_mode),
+                    contentDescription = stringResource(R.string.dark_mode_icon),
+                    modifier = Modifier
+                        .size(24.dp)
+                        .padding(horizontal = 20.dp)
+                )
+                Image(
+                    painter = painterResource(R.drawable.ic_recipes),
+                    contentDescription = stringResource(R.string.recipes_icon),
+                    modifier = Modifier
+                        .size(24.dp)
+                        .padding(horizontal = 20.dp)
+                )
+            }
+        }
+    )
 }
 
 @Composable
@@ -40,8 +78,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MyToolbarPreview() {
     TapTaOrdersTheme {
-        Greeting("Android")
+        MyToolbar()
     }
 }
