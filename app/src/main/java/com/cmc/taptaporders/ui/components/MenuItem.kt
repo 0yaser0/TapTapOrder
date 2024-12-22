2e4855c80441d613fc83a263c9b986583467a898
Yaser yjjou <125563605+0yaser0@users.noreply.github.com>
2024-12-22 23:02:47 +0100

    Update UI for Menu Page

---
+++ app/src/main/java/com/cmc/taptaporders/ui/components/MenuItem.kt
@@ -0,0 +1,105 @@
+package com.cmc.taptaporders.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun MenuItem(
+    imageResId: Int,
+    title: String,
+    description: String,
+    onClick: () -> Unit
+) {
+    val context = LocalContext.current
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                15.dp
+            )
+            .clickable {
+                onClick()
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(
+                id = imageResId
+            ), contentDescription = null,
+            modifier = Modifier.size(40.dp)
+        )
+        Spacer(
+            modifier = Modifier.width(8.dp)
+        )
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = title, style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+        Spacer(
+            modifier = Modifier.width(8.dp)
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = { /* Decrement logic here */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF00AAFF)),
+                shape = RoundedCornerShape(4.dp),
+                contentPadding = PaddingValues(0.dp),
+                modifier = Modifier.size(20.dp)
+            ) {
+                Text(
+                    "-",
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "0",
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(
+                onClick = { /* Increment logic here */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF00AAFF)),
+                shape = RoundedCornerShape(4.dp),
+                contentPadding = PaddingValues(0.dp),
+                modifier = Modifier.size(20.dp)
+            ) {
+                Text(
+                    "+",
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+        }
+
+    }
+}
