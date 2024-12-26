3225f9e2718c340d49584f61dc9de4a55984c5ab
Yaser yjjou <125563605+0yaser0@users.noreply.github.com>
2024-12-26 10:12:44 +0100

    added items of category and menu

--- app/src/main/java/com/cmc/taptaporders/ui/CategoryPage.kt
+++ app/src/main/java/com/cmc/taptaporders/ui/CategoryPage.kt
@@ -1,87 +1,22 @@
 package com.cmc.taptaporders.ui
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.cmc.taptaporders.ui.components.MyToolbar
-import com.cmc.taptaporders.data.local.models.CategoryItem
-import com.cmc.taptaporders.ui.components.CategoryItem
-import com.cmc.taptaporders.ui.components.GetTicket
-import com.cmc.taptaporders.ui.components.SearchBarCall
-import com.cmc.taptaporders.ui.components.TotalPrice
-import com.cmc.taptaporders.ui.theme.TapTaOrdersTheme
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.taptaorders.R
-    
-@Composable
-fun CategoryPage() {
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = {
-            Column {
-                MyToolbar()
-                SearchBarCall()
-            }
-        },
-        content = { paddingValues ->
-            Box(modifier = Modifier.fillMaxSize()) {
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(paddingValues)
-                ) {
 
-                    val categoryList = listOf(
-                        CategoryItem(R.drawable.burger, "Burger", "bnin"),
-                        CategoryItem(R.drawable.burger, "Burger", "nadi"),
-                        CategoryItem(R.drawable.burger, "Burger", "nadi"),
-                        CategoryItem(R.drawable.burger, "Burger", "nadi"),
-                        CategoryItem(R.drawable.burger, "Burger", "nadi"),
-                        CategoryItem(R.drawable.burger, "Burger", "nadi"),
-                        CategoryItem(R.drawable.burger, "Burger", "nadi"),
-                        CategoryItem(R.drawable.burger, "Burger", "nadi"),
-                        CategoryItem(R.drawable.burger, "Burger", "nadi"),
-                        CategoryItem(R.drawable.burger, "Tacos", "harban")
-                    )
-
-                    items(categoryList) { food ->
-                        CategoryItem(
-                            imageResId = food.path,
-                            title = food.title,
-                            description = food.desc,
-                            onClick = {}
-                        )
-                    }
-                }
-
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .align(Alignment.BottomCenter)
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        TotalPrice()
-                        GetTicket()
-                    }
-                }
-            }
+class CategoryPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_category_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun CategoryPagePreview() {
-    TapTaOrdersTheme {
-        CategoryPage()
     }
 }
